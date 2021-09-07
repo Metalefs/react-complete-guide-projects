82b13da56dbc9f8f8650b48c94eadcd292add677
Hermes <jack-ten@hotmail.com>
2021-09-07 16:32:18 -0300

    using portals on modal

--- react-complete-guide-add-users/src/components/UI/Modal.js
+++ react-complete-guide-add-users/src/components/UI/Modal.js
@@ -1,26 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import styles from "./Modal.module.css";
+
 import Button from "./Button";
 import Card from "./Card";
 
+const Backdrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onConfirm} />;
+  };
+  
+  const ModalOverlay = (props) => {
+    return (
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    );
+  };
+  
 function Modal(props) {
 
-    function closeModalHandler(){
-        props.close(true);
-    }
-
-    return  <div className={styles.backdrop} onClick={closeModalHandler}>
-               <div style={{height:"100%", display:"flex"}}>
-                    <Card className={styles.modal}>
-                        <nav className={styles.header}><h2>{props.title}</h2></nav>
-                        <div className={styles.content}>
-                            {props.children}
-                            <footer className={styles.actions}>
-                                <Button onClick={closeModalHandler}>Okay</Button>
-                            </footer>
-                        </div>
-                    </Card> 
-               </div>
-            </div>
+    return (
+        <React.Fragment>
+          {ReactDOM.createPortal(
+            <Backdrop onConfirm={props.close} />,
+            document.getElementById('backdrop-root')
+          )}
+          {ReactDOM.createPortal(
+            <ModalOverlay
+              title={props.title}
+              message={props.children}
+              onConfirm={props.close}
+            />,
+            document.getElementById('overlay-root')
+          )}
+        </React.Fragment>
+      );
 }
 
 export default Modal;
